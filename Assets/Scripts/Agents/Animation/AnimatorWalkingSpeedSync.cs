7abb725a58f42324944faf9b9b84d9ad0071a4dd
Jan <8350867+EmanueleGiannuzzi@users.noreply.github.com>
2024-03-10 16:23:02 +0100

    Agent Animations

---
+++ Assets/Scripts/Agents/Animation/AnimatorWalkingSpeedSync.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator), typeof(NavMeshAgent))]
+public class AnimatorWalkingSpeedSync : MonoBehaviour {
+    private Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        float speed = navMeshAgent.desiredVelocity.magnitude;
+        animator.speed = speed / 2f; // The walking animation plays at 2m/s at 1x speed
+        animator.SetBool(0, speed > 0.2f); //"IsWalking" parameter
+    }
+}
