c5f332b11990a838dced96e26c3cbfbe04bc8440
Jan <8350867+EmanueleGiannuzzi@users.noreply.github.com>
2024-10-14 22:57:39 +0200

    time controller fixes

--- Assets/Scripts/Utility/TimeController.cs
+++ Assets/Scripts/Utility/TimeController.cs
@@ -2,6 +2,8 @@
 
 public class TimeController : MonoBehaviour {
     public int TargetFPS = 30;
+    public float MaxMultiplier = 10f;
+    public float MaxIncrementPerFrame = 0.5f;
 
     private const int FPS_BUFFER_SIZE = 10;
     private const float UPDATE_FREQUENCY = 1f / FPS_BUFFER_SIZE;
@@ -42,7 +44,11 @@ public class TimeController : MonoBehaviour {
     private void AdjustTimeScale(float avgFPS) {
         float scaleFactor = avgFPS / TargetFPS;
         float newTimeScale = Time.timeScale * scaleFactor;
-        Time.timeScale = Mathf.Clamp(newTimeScale, 1f, 100f);
+
+        float timeScaleDifference = newTimeScale - Time.timeScale;
+        timeScaleDifference = Mathf.Clamp(timeScaleDifference, -MaxIncrementPerFrame, MaxIncrementPerFrame);
+        Time.timeScale += timeScaleDifference;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 1f, MaxMultiplier);
 
         Multiplier = Time.timeScale;
     }
